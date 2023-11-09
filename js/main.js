65d48c8bbfb89f9bcc077c3617d06a3f99637691
Romina Carranza <romii.carranza@gmail.com>
2023-11-08 21:58:56 -0300

    agregando JS en el form

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const inputNombre = document.getElementById("nombre")
+const inputEmail = document.getElementById("email")
+const inputTel = document.getElementById("telefono")
+const inputMensaje = document.getElementById("mensaje")
+const pattern =/[a-z0-9._%\-]+@[a-z]+\.[a-z]{3}$/
+const patternNum = /^\d+$/
+
+function validarEntrada() {
+    let nombre = inputNombre.value
+    if(nombre.length < 3) {
+        inputNombre.classList.add("inputNoValido")
+    } else {
+        inputNombre.classList.remove("inputNoValido")
+    }
+}
+
+function validarEmail() {
+    let email = inputEmail.value
+    if(!pattern.test(email)) {
+        inputEmail.classList.add("inputNoValido")
+    } else {
+        inputEmail.classList.remove("inputNoValido")
+    }
+}
+
+function validarTel() {
+    let tel = inputTel.value
+    if( patternNum.test(tel) && tel.length <= 12 && tel.length >= 6){
+        inputTel.classList.remove("inputNoValido")
+    } else {
+        inputTel.classList.add("inputNoValido")
+    }
+
+}
